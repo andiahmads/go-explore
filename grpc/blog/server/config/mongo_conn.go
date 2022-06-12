da076302052c136751c169540ebfd13d4da06eab
andiahmads <andi.fivesco@gmail.com>
2022-06-13 00:18:26 +0700

    add mongo connection

---
+++ grpc/blog/server/config/mongo_conn.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/writeconcern"
+)
+
+var mgo *mongo.Client
+var err error
+
+func Init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	mongoConn := "mongodb://admin:endi@localhost:27017/"
+	clientOptions := options.Client().ApplyURI(mongoConn).SetWriteConcern(writeconcern.New(writeconcern.W(1), writeconcern.J(false)))
+	// clientOptions := options.Client().ApplyURI(os.Getenv("MONGO")).SetWriteConcern(writeconcern.New(writeconcern.W(1), writeconcern.J(false)))
+	mgo, err = mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+		panic(err)
+	}
+	err = mgo.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+		panic(err)
+	}
+	// fmt.Println("mongo connect", mgo)
+
+}
+
+func GetDB() *mongo.Client {
+	return mgo
+}
